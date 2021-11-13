2262640806752e42e67b96367468ff85050abdb3
Angela Loftus <angelaloftus@yahoo.com>
2021-11-13 13:28:34 -0600

    render HTML templates

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const Engineer = require('./lib/Engineer');
 let engineersArr = [];
 let internsArr = [];
 let managersArr = [];
-
-
+//one manager (reorganize later)
+//template literal for engineers and interns 
 
 //call the function to start the prompts
 chooseEmployee();
@@ -52,7 +52,8 @@ function chooseEmployee() {
             createManager();
             break;
         case "Exit": 
-            // createHtml();
+            writeToFile();
+            
             console.log("Team successfully created. Goodbye.")
             break; 
         }
@@ -68,8 +69,8 @@ function createEngineer() {
         },
         { 
             type: 'number',
-            name: 'salary',
-            message: "Enter the engineer's salary as a number: ",
+            name: 'id',
+            message: "Enter the engineer's id as a number: ",
         },
         {
             type: "input",
@@ -83,13 +84,13 @@ function createEngineer() {
         }
     ]).then(res => {
         console.log(res);
-        if(isNaN(res.salary)) {
-            console.log("Error, you must enter a number for your salary");
+        if(isNaN(res.id)) {
+            console.log("Error, you must enter a number for your id");
             console.log("try again");
             createEngineer();
         }
 
-        const engineer = new Engineer(res.name, res.salary, res.email, res.github);
+        const engineer = new Engineer(res.name, res.id, res.email, res.github);
         engineersArr.push(engineer);
         console.log(engineersArr);
         chooseEmployee();
@@ -105,8 +106,8 @@ function createIntern() {
         },
         { 
             type: 'number',
-            name: 'salary',
-            message: "Enter the intern's salary as a number: ",
+            name: 'id',
+            message: "Enter the intern's id as a number: ",
         },
         {
             type: "input",
@@ -120,13 +121,13 @@ function createIntern() {
         }
     ]).then(res => {
         console.log(res);
-        if(isNaN(res.salary)) {
-            console.log("Error, you must enter a number for your salary!");
+        if(isNaN(res.id)) {
+            console.log("Error, you must enter a number for your id!");
             console.log("try again");
             createIntern();
         }
 
-        const intern = new Intern(res.name, res.salary, res.email, res.school);
+        const intern = new Intern(res.name, res.id, res.email, res.school);
         internsArr.push(intern);
         console.log(internsArr);
         chooseEmployee();
@@ -142,8 +143,8 @@ function createManager() {
         },
         { 
             type: 'number',
-            name: 'salary',
-            message: "Enter the manager's salary as a number: ",
+            name: 'id',
+            message: "Enter the manager's id as a number: ",
         },
         {
             type: "input",
@@ -157,22 +158,120 @@ function createManager() {
         }
     ]).then(res => {
         console.log(res);
-        if(isNaN(res.salary)) {
-            console.log("Error, you must enter a number for your salary!");
+        if(isNaN(res.id)) {
+            console.log("Error, you must enter a number for your id!");
             console.log("try again");
             createManager();
         }
-
-        const manager = new Manager(res.name, res.salary, res.email, res.officeNumber);
+        
+        const manager = new Manager(res.name, res.id, res.email, res.officeNumber);
         managersArr.push(manager);
         console.log(managersArr);
         chooseEmployee();
     })
 }
 
-console.log(managersArr);
-console.log(engineersArr);
-console.log(internsArr);
+function renderEngineerHtml() {
+    //map is a loop
+    let templateEngineers = engineersArr.map(engineer => {
+        //return the template literal for the card
+        return  `<div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Engineer<br>${engineer.name}</h5>
+                        <p class="card-text">
+                        Id: ${engineer.id}
+                        <br>Email: <a href = "mailto: ${engineer.email}">${engineer.email}</a> 
+                        <br> Github:<a href= "${engineer.github}" class="card-link" target="_blank">${engineer.github}</a></p>                        
+                    </div>
+                </div>`
+    })
+    console.log(templateEngineers);
+    return templateEngineers.join(''); 
+}
+
+function renderInternHtml() {
+    let templateInterns = internsArr.map(intern => {
+        return  `<div class="card" style="width: 18rem;">
+                    <div class="card-body">s
+                        <h5 class="card-title">Intern<br>${intern.name}</h5>
+                        <p class="card-text">
+                        Id: ${intern.id}
+                        <br>Email: <a href = "mailto: ${intern.email}">${intern.email}</a> 
+                        <br>School: ${intern.school}</p>
+                    </div>
+                </div>`
+    });
+    console.log(templateInterns);
+    return templateInterns.join('');
+};
+
+function renderManagerHtml() {
+    let templateManagers = managersArr.map(manager => {
+        return `<div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Manager<br>${manager.name}</h5>
+                        <p class="card-text">
+                        Id: ${manager.id}
+                        <br>Email: ${manager.email}
+                        <br>Email: <a href = "mailto: ${manager.email}">${manager.email}</a> 
+                        <br> Office Number: ${manager.officeNumber}</p>
+                    </div>
+                </div>`
+    })
+    console.log(templateManagers);
+    return templateManagers.join('');
+}
+
+//function for all of the html generation (engineer, manager, intern)
+//return a big template literal
+function createHtml () {
+    //Copy boilerplate html that refers to the stylesheet
+    return `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>HTML 5 Boilerplate</title>
+        <link rel= "stylesheet" href = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+      </head>
+      <header class= "bg-danger" >
+        <h1> My Team </h1>
+      </header>
+      <body>
+        <div class ="container" >
+            <div class = "row">
+                <div class = "col-sm-4">
+                    ${renderManagerHtml()}
+                </div>
+                <div class = "col-sm-4">
+                    ${renderEngineerHtml()}
+                </div>            
+                <div class = "col-sm-4">
+                    ${renderInternHtml()}
+                </div>
+            </div>
+        </div>
+        <script src="index.js"></script>
+      </body>
+    </html>
+         `
+}
+
+function writeToFile() {
+
+    fs.writeFile("./dist/index.html", createHtml(), err => {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Employee HTML was created successfully.");
+    });
+}
+
+// console.log(managersArr);
+// console.log(engineersArr);
+// console.log(internsArr);
 // if (getManager() === true) {
 //     inquirer.prompt {}
 // }
